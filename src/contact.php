d42edce71056e72a817b5c76f50ef178e03ceb2c
Irfanadl <Irfan.adl410622@gmail.com>
2021-12-20 15:15:06 +0800

    Contact Page Update & Footer
    New contact page.
    Added footer.php

---
+++ src/contact.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+  <link rel="stylesheet" href="contact.css">
+  <title>Pustaka Ilmiah</title>
+</head>
+<body>
+
+  <!-- GoogleMaps -->
+  <iframe class="min-vh-50 w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.7512199504536!2d103.13602981404495!3d5.301464737633199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31b7be521f626f6d%3A0x3696394ac71a3ad2!2sPustaka%20Ilmiah!5e0!3m2!1sen!2smy!4v1639968124196!5m2!1sen!2smy" loading="lazy"></iframe>
+  <!-- End of GoogleMaps -->
+
+
+  <section class="min-vh-50 w-100 p-3">
+    <div class="container d-md-flex justify-content-around">
+
+      <!-- Accordion -->
+      <div class="vw-md-40 p-3 m-1">
+        <h6>FREQUENTLY ASKED QUESTIONS</h6>
+        <hr>
+        <div class="accordion accordion-flush" id="accordionFlush">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                What are the delivery charges for orders from the Online Shop?
+              </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlush">
+              <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit quae veritatis modi aperiam aliquam ad soluta voluptate atque reprehenderit.</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                Which payment methods are accepted in the Online Shop?
+              </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlush">
+              <div class="accordion-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem quisquam quibusdam voluptate porro aut!</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                How long will delivery take?
+              </button>
+            </h2>
+            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlush">
+              <div class="accordion-body">Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                How secure is shopping in the Online Shop? Is my data protected?
+              </button>
+            </h2>
+            <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlush">
+              <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ducimus molestiae sit beatae porro mollitia! Iste dolores nobis pariatur ex odio alias? Id, voluptatum reiciendis?</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                What exactly happens after ordering?
+              </button>
+            </h2>
+            <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlush">
+              <div class="accordion-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus nisi, saepe inventore vel quis quod rem, unde quos natus ad repellendus dolores voluptas esse, ducimus culpa aliquam neque possimus similique.</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                Do i receive an invoice for my order?
+              </button>
+            </h2>
+            <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlush">
+              <div class="accordion-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error exercitationem blanditiis dignissimos, labore maiores repudiandae!</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End of Accordion -->
+
+      <!-- Form -->
+      <div class="vw-md-40 p-3 m-1">
+        <h6>SEND US AN EMAIL</h6>
+        <hr>
+        <div class="w-100">
+          <label class="p-1" for="name">Your Name (required)</label>
+          <input class="w-100 p-2 shadow-sm" type="text" name="name" id="name" required>
+          <label class="p-1" for="email">Your Email (required)</label>
+          <input class="w-100 p-2 shadow-sm" type="text" name="email" id="email" required>
+          <label class="p-1" for="message">Your Message (required)</label>
+          <textarea class="w-100 p-2 shadow-sm" type="text" name="message" id="message" required></textarea>
+        </div>
+        
+      </div>
+      <!-- End of Form -->
+
+    </div>
+  </section>
+  
+  <hr>
+  
+  <!-- Footer -->
+  <div id="footer"></div>
+  <!-- End of Footer -->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script>
+    $("#footer").load("footer.php"); 
+  </script>
+</body>
+</html>
---
+++ src/footer.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="contact.css">
+  <title>Document</title>
+</head>
+<body>
+  <section class="min-vh-50 w-100 p-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg">
+          <p class="fw-bold text-muted">LATEST</p>
+          <hr>
+
+          <!-- Item -->
+          <a href="#" class="text-decoration-none text-black">
+            <div class="shadow-sm m-1">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img src="img/5-768x1024.jpg" alt="" class="img-thumbnail footer-item-img m-1">
+                </div>
+                <div class="ml-2">
+                  <p class="mx-2 item-text">KEMBALI KEPADA JAWI : PANDAI MEMBACA DAN MENULIS JAWI DALAM MASA 20 JAM</p>
+                  <p class="mx-2 fw-bold">RM5.00</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <!-- End of Item -->
+
+          <!-- Item -->
+          <a href="#" class="text-decoration-none text-black">
+            <div class="shadow-sm m-1">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img src="img/25.jpg" alt="" class="img-thumbnail footer-item-img m-1">
+                </div>
+                <div class="ml-2">
+                  <p class="mx-2 item-text">Ubah Patah Hati Jadi Prestasi</p>
+                  <p class="mx-2 fw-bold">RM40.00</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <!-- End of Item -->
+
+        </div>
+        <div class="col-12 col-md-6 col-lg">
+          <p class="fw-bold text-muted">BEST SELLING</p>
+          <hr>
+          <!-- Item -->
+          <a href="#" class="text-decoration-none text-black">
+            <div class="shadow-sm m-1">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img src="img/25.jpg" alt="" class="img-thumbnail footer-item-img m-1">
+                </div>
+                <div class="ml-2">
+                  <p class="mx-2 item-text">Ubah Patah Hati Jadi Prestasi</p>
+                  <p class="mx-2 fw-bold">RM40.00</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <!-- End of Item -->
+          <!-- Item -->
+          <a href="#" class="text-decoration-none text-black">
+            <div class="shadow-sm m-1">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img src="img/5-768x1024.jpg" alt="" class="img-thumbnail footer-item-img m-1">
+                </div>
+                <div class="ml-2">
+                  <p class="mx-2 item-text">KEMBALI KEPADA JAWI : PANDAI MEMBACA DAN MENULIS JAWI DALAM MASA 20 JAM</p>
+                  <p class="mx-2 fw-bold">RM5.00</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <!-- End of Item -->
+        </div>
+        <div class="col-12 col-md-6 col-lg">
+          <p class="fw-bold text-muted">TOP RATED</p>
+          <hr>
+          <!-- Item -->
+          <a href="#" class="text-decoration-none text-black">
+            <div class="shadow-sm m-1">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img src="img/25.jpg" alt="" class="img-thumbnail footer-item-img m-1">
+                </div>
+                <div class="ml-2">
+                  <p class="mx-2 item-text">Ubah Patah Hati Jadi Prestasi</p>
+                  <p class="mx-2 fw-bold">RM40.00</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <!-- End of Item -->
+          <!-- Item -->
+          <a href="#" class="text-decoration-none text-black">
+            <div class="shadow-sm m-1">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img src="img/5-768x1024.jpg" alt="" class="img-thumbnail footer-item-img m-1">
+                </div>
+                <div class="ml-2">
+                  <p class="mx-2 item-text">KEMBALI KEPADA JAWI : PANDAI MEMBACA DAN MENULIS JAWI DALAM MASA 20 JAM</p>
+                  <p class="mx-2 fw-bold">RM5.00</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <!-- End of Item -->
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <!-- Footer -->
+  <footer class="text-center text-lg-start bg-grey-77 text-light">
+    <!-- Section: Links  -->
+    <section class="p-2">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              About Us
+            </h6>
+            <hr>
+            <p>
+            Kedai Buku, Al-Quran & Alat Tulis dan Servis Perkhidmatan antaranya :<br>
+            (1) Buku Terkini & Bestseller<br>
+            (2) Bahan Perpustakaan<br>
+            (3) Alat Bantu Mengajar<br>
+            (4) Alat Tulis<br>
+            (5) Makanan Sunnah<br>
+            </p>
+            <div>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-whatsapp"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-telegram-plane"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="far fa-envelope"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="" class="socmed me-4 text-reset text-decoration-none">
+                <i class="fab fa-pinterest"></i>
+              </a>
+            </div>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Latest News
+            </h6>
+            <hr>
+            <p>
+              <a href="#" class="news-link text-light text-decoration-none">
+                <span class="font-monospace">6th May - </span>
+                Website Launched!
+              </a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Sign Up For Newsletter
+            </h6>
+            <hr>
+            <form>
+              <input type="text" class="rounded-pill mb-2 p-1" name="email">
+              <button class="btn bg-maroon rounded-pill p-2 text-light">Sign Up</button>
+            </form>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Contact
+            </h6>
+            <p><i class="fas fa-home me-3"></i> Pustaka Ilmiah, 1150-a, Jalan Pasir Panjang, 21100 Kuala Terengganu, Terengganu</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              pustaka_ilmiah@yahoo.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> 09-6226996</p>
+            <p><i class="fas fa-print me-3"></i> 09-6226996</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4 bg-grey-5b d-md-flex justify-content-between">
+      <div>
+        Copyright Pustaka Ilmiah ©️ 2021 |
+        <a class="text-reset fw-bold" href="#">website.com</a>
+      </div>
+      <div>
+        <i class="payment-icon mx-1 fab fa-cc-visa"></i>
+        <i class="payment-icon mx-1 fab fa-cc-mastercard"></i>
+        <i class="payment-icon mx-1 fas fa-money-check"></i>
+      </div>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- End Footer -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
